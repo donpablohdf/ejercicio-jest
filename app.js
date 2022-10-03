cca1f5c0c002a41d90a335f0ddd22fbb07025260
Don Pablo el de Felicitas <pabloalegrejuan@gmail.com>
2022-10-03 15:22:48 +0000

    Finalizado

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // one euro is:
 const unEuroEs = {
-    "JPY": 141.93, // yen
+    "JPY": 141.52, // yen
     "USD": 0.98, // dolar
-    "GBP": 0.88, // libra
+    "GBP": 0.87, // libra
 }
 const fromEuroToDollar = function(valueInEuro){
     // convertimos el valor a dolares
@@ -14,23 +14,14 @@ const fromEuroToDollar = function(valueInEuro){
 }
 const fromDollarToYen = function(valueInDollar){
     
-    let valueInYen = valueInDollar / unEuroEs["JPY"];
-    valueInYen = valueInYen.toFixed(4);
-    
+    let valueInYen = valueInDollar * (unEuroEs["USD"]/unEuroEs["JPY"]);
+    valueInYen = valueInYen.toFixed(4);    
     return valueInYen;
 }
 const fromYenToPound = function(valueInYen){
     
-    let valueInPound = valueInYen * (unEuroEs["GBP"]/100);
+    let valueInPound = valueInYen * (unEuroEs["GBP"]/unEuroEs["JPY"]);
     valueInPound = valueInPound.toFixed(4);
     return valueInPound;
 }
-let valor_euroDolar= 1* unEuroEs["USD"];
-let valor_DolarYen= 1 * unEuroEs["JPY"];
-let valor_YenPound= 1 * unEuroEs["GBP"];
-
-console.log("un dolar es "+valor_euroDolar+" euro" )
-console.log("un yen es "+ valor_DolarYen+" dolar")
-console.log("un yen es "+ valor_YenPound+" libras")
-
-module.exports = { fromEuroToDollar, fromDollarToYen, fromYenToPound}
+module.exports = { unEuroEs, fromEuroToDollar, fromDollarToYen, fromYenToPound}
--- test.js
+++ test.js
@@ -1,8 +1,4 @@
-const unEuroEs = {
-    "JPY": 141.93, // yen
-    "USD": 0.98, // dolar
-    "GBP": 0.88, // libra
-}
+const { unEuroEs} = require('./app.js');
 
 
 test("Un euro es 0.9800 dolares", function(){
@@ -10,24 +6,24 @@ test("Un euro es 0.9800 dolares", function(){
     // uso la funcion
     const dolares = fromEuroToDollar(1)
     //calculo el resultado esperado
-    let resultado = 1 * unEuroEs["USD"];
+    let resultado = 1* unEuroEs["USD"];
     resultado = resultado.toFixed(4);
     // comparo con Jest
     expect(resultado).toBe(dolares);
 })
 
-test("Un dolar son 0.0070 Yenes", function(){
+test("Un dolar son 0.0069 Yenes", function(){
     const {fromDollarToYen} = require('./app.js');
     const yenes = fromDollarToYen(1)
-    let resultado = 1 / unEuroEs["JPY"];
+    let resultado = 1 * (unEuroEs["USD"]/unEuroEs["JPY"])
     resultado = resultado.toFixed(4);
     expect(resultado).toBe(yenes);
 })
 
-test("Un yen son 0.0088 libras", function(){
+test("Un yen son 0.0061 libras", function(){
     const { fromYenToPound } = require('./app.js');
     const libras = fromYenToPound(1)
-    let resultado = 1 * (unEuroEs["GBP"]/100);
+    let resultado = 1 * (unEuroEs["GBP"]/unEuroEs["JPY"]);
     resultado = resultado.toFixed(4); 
     expect(resultado).toBe(libras);
 })
